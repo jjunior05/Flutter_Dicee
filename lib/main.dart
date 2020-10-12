81dc802ba776cd1085db2a27138e80233c87cbc5
jjunior05 <jjquelito@gmail.com>
2020-10-12 14:15:36 -0300

    Início projeto

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
+          centerTitle: true,
           title: Text('Dicee'),
           backgroundColor: Colors.red,
         ),
